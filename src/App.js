949662105fe410cb33b6ef3cd7b75e789957d296
Pedro Henrique Reis <72507155+PReis1@users.noreply.github.com>
2020-11-25 19:29:55 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -42,16 +42,15 @@ function App() {
           {repository.title}
 
           <button onClick={() => handleRemoveRepository(repository.id)}>
-            Remove
+            Remover
         </button>
         </li>
         ))}
       </ul>
 
-      <button onClick={handleAddRepository}>Add</button>
+      <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
 }
 
 export default App;
-
